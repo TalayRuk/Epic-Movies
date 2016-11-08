8c0efe2f6425fd24c5af5bc2bfeefe4f0fd9adef
rouz majlessi <rmajlessi@gmail.com>
2016-11-08 12:10:55 -0800

    add action to contact-page file

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  name: "peter",
-  actions: {
-    post() {
-      if (confirm('Your information has been sent.')){
-      }
-      this.set('name', 'hey');
-    }
-  }
 });
