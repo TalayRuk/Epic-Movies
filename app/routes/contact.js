48ee88d36c98e0a2c1be6901cdf263e1f7978b33
rouz majlessi <rmajlessi@gmail.com>
2016-11-08 11:05:39 -0800

    add confrim action too contact form

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    post(contact) {
+      if (confirm('Your information has been sent.')){
+        this.sendAction('destroyContact', contact);
+      }
+    }
+  }
 });
