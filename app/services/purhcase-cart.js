d2a21001f30d1b1618ee39554964902e526330b0
rouz majlessi <rmajlessi@gmail.com>
2016-11-08 16:25:34 -0800

    resolve merge conflicts

--- app/services/purhcase-cart.js
+++ app/services/purhcase-cart.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  tickets: [],
+
+  add(tickets) {
+    var ticket = {
+      this.get('title'),
+      this.get('cost'),
+      this.get('quantity'),
+      this.get('type'),
+    };
+    this.get('tickets').pushObject(ticket);
+  }
+
 });
