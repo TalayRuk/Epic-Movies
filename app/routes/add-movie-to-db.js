dbf6ff8e6ca68713ec0d966b5104ec3f3dc728be
ian Wilcox <ian Wilcox>
2016-11-08 17:07:06 -0800

    can call and add a movie with prepopulated values. option to change-no longer necessary

--- app/routes/add-movie-to-db.js
+++ app/routes/add-movie-to-db.js
@@ -1,10 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
- model: function(params) {
-   var url = 'http://www.omdbapi.com/?t=' + params.newMovie +'&y=&plot=short&r=json';
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-     return responseJSON;
-   });
+  model: function(params) {
+    var url = 'http://www.omdbapi.com/?t=' + params.newMovie +'&y=&plot=short&r=json';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON;
+    });
+  },
+  actions: {
+    saveMovieToDB(params) {
+
+      console.log(params);
+
+      var newMovie = this.store.createRecord('movie', params);
+      newMovie.save();
+      this.transitionTo('index');
+
+    }
+
   }
 });
