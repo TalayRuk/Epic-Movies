2651ec319e90a8dbd4d86a34eea6ea9e55f05ccc
rouz majlessi <rmajlessi@gmail.com>
2016-11-10 11:13:49 -0800

    add delete too cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,5 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   purchaseCart: Ember.inject.service(),
+  actions: {
+    deleteTicket(ticket){
+      this.get('purchaseCart').deleteTicket(ticket);
+    }
+  }
 
 });
