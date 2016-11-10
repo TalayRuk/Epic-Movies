9d55a0e96425748f1c2f703fe00751c4bd6cd833
rouz majlessi <rmajlessi@gmail.com>
2016-11-09 16:24:13 -0800

    master -delete commit

--- app/routes/theaters.js
+++ app/routes/theaters.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-11-10&api_key=ryhrbug5pq4222vswnsd48z2&zip=98126';
+    var url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-11-10&api_key=9czrueubd565sr4gteqyhtzv&zip=98126';
 
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON);
